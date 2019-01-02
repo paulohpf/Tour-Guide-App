63f402ba8d48bb94c1ac76fe02068c4612d37d5b
Paulo Henrique Palmeira Franco <paulohpfranco@uol.com.br>
2019-01-02 13:59:13 -0200

    Correções revisão #1644075

--- app/src/main/java/br/com/paulohpfranco/tourguideapp/PlacesListAdapter.java
+++ app/src/main/java/br/com/paulohpfranco/tourguideapp/PlacesListAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,6 +32,19 @@ public class PlacesListAdapter extends ArrayAdapter<Place> {
 
         // Get the {@link Word} object located at this position in the list
         Place currentPlace = getItem(position);
+
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = listItemView.findViewById(R.id.imageViewListItem);
+        // Check if an image is provided for this word or not
+        if (currentPlace.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentPlace.getmPlaceImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
 
         // Find the TextView in the list_item.xml layout with the ID textViewListItem.
         TextView textViewListItem = listItemView.findViewById(R.id.textViewListItem);
